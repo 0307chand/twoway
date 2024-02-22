3de7614d2d81abb97f58d39601a89688d6848c23
chandini02 <chandini0703@gmail.com>
2024-02-22 10:42:16 +0530

    intial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'twoway';
+  name!: string;
+  em!: string;
+  pass!:string;
+  show(){
+    alert("successfully submitted")
+  }
 }
